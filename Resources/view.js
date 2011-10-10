620556ef75b342ffe483eb12a73bfec3a3fba386
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-10-10 13:44:30 +0900

    トレーニング記録詳細画面でダミーじゃなくてほんとの日付でとるようになった

--- Resources/view.js
+++ Resources/view.js
@@ -9,7 +9,7 @@ var data = [];
 
 var db = Ti.Database.open('gymreco');
 db.execute('CREATE TABLE IF NOT EXISTS TRAINNINGS (type INTEGER, value INTEGER, created_at TEXT)');
-var rows = db.execute("SELECT * FROM TRAINNINGS WHERE created_at = '2011-10-10'");
+var rows = db.execute("SELECT * FROM TRAINNINGS WHERE created_at = ?", win.title);
 while (rows.isValidRow()) {
     Ti.API.info(' CREATED_AT: ' + rows.fieldByName('created_at') +
                 ' TYPE: ' + rows.fieldByName('type') +
