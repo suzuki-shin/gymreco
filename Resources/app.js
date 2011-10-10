6aa55d95c62d2351de537ccfb43dafc9884a0713
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-10-10 20:15:48 +0900

    ちょいrefactoring

--- Resources/app.js
+++ Resources/app.js
@@ -2,37 +2,44 @@ Ti.UI.setBackgroundColor('#000');
 
 var tabGroup = Ti.UI.createTabGroup();
 
-var win_record = Ti.UI.createWindow({
-    title:'記録する',
-    backgroundColor:'#fff',
-    url:'win_record.js'
-});
 var tab1 = Ti.UI.createTab({
     icon:'KS_nav_views.png',
-    title:'記録する',
-    window:win_record
+    title:'記録',
+    window:Ti.UI.createWindow({
+        title:'記録',
+        backgroundColor:'#fff',
+        url:'win_record.js'
+    })
 });
 
-var win_view = Ti.UI.createWindow({
-    title:'記録を見る',
-    backgroundColor:'#fff',
-    url:'win_view.js'
-});
 var tab2 = Ti.UI.createTab({
     icon:'KS_nav_ui.png',
-    title:'記録を見る',
-    window:win_view
+    title:'履歴',
+    window:Ti.UI.createWindow({
+        title:'履歴',
+        backgroundColor:'#fff',
+        url:'win_view.js'
+    })
 });
 
-var debug = Ti.UI.createWindow({
-    title:'DEBUG',
-    backgroundColor:'#fff',
-    url:'debug.js'
-});
 var tab3 = Ti.UI.createTab({
     icon:'KS_nav_ui.png',
+    title:'設定',
+    window:Ti.UI.createWindow({
+        title:'設定',
+        backgroundColor:'#fff',
+        url:'win_config.js'
+    })
+});
+
+var tab4 = Ti.UI.createTab({
+    icon:'KS_nav_ui.png',
     title:'DEBUG',
-    window:debug
+    window:Ti.UI.createWindow({
+        title:'DEBUG',
+        backgroundColor:'#fff',
+        url:'debug.js'
+    })
 });
 
 tabGroup.addTab(tab1);
